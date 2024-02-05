3c15e4a1306b817cc5fa9ec2146f0f7f499a3396
DarkWood1558 <maurice.dd@icloud.com>
2024-02-05 19:19:11 +0100

    commit

---
+++ express-server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require("path");
+const portnumber = 4009;
+const server = express();
+var url = require('url')
+
+
+server.use(express.static("public_html"));
+
+server.get('/random.html', sendRandom);
+
+server.get('/suchen', suchen);
+
+function suchen(req, res) {
+
+    let parsedUrl = url.parse(req.url,true);
+
+    console.log(`${req.url} requested`);
+    res.send(`Ihr Inhalt war ${parsedUrl.query["inhalt"]}`);
+}
+
+
+function sendRandom(req, res) {
+    console.log('/random.html requested');
+    let x = Math.random();
+    let answer = 
+    `
+<html>
+<body>    
+    <h2>this is a random number: <code>${x}</code></h2>
+</body>
+</html>`;
+    res.send(answer);
+}
+
+
+
+
+server.listen(portnumber, function ()  {
+    console.log('listening at port ' + portnumber);
+});
